65b8ed4d00053c10ca49632b1be62541a9a6a909
Raquel Sato <raquelsato1@gmail.com>
2024-08-22 17:38:50 -0300

    feat: add modal on dashboard actions

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -4,25 +4,45 @@ import Button from "~/components/Buttons";
 
 type Prop = {
   title: React.ReactNode;
+  description: string;
   isOpen: boolean;
   onDismiss: () => unknown;
   action: () => void;
 };
 
-const Modal = ({ isOpen = false, onDismiss, title, action }: Prop) => {
+const Modal = ({
+  isOpen = false,
+  onDismiss,
+  title,
+  description,
+  action,
+}: Prop) => {
   return (
     <S.Overlay onClick={onDismiss} $isOpen={isOpen}>
-      <S.Content onClick={(e: Event) => e.stopPropagation()}>
+      <S.Content
+        onClick={(e: Event) => e.stopPropagation()}
+      >
         <S.Header>
           <S.Title>{title}</S.Title>
           <S.CloseWrapper>
-            <MdClose aria-label="Fechar modal" onClick={onDismiss} />
+            <MdClose
+              aria-label="Fechar modal"
+              onClick={onDismiss}
+            />
           </S.CloseWrapper>
         </S.Header>
-        <S.Description>Deseja confirmar a ação?</S.Description>
+        <S.Description>
+          {description
+            ? description
+            : "Deseja confirmar a ação?"}
+        </S.Description>
         <S.ButtonsWrapper>
           <Button onClick={action}>Sim</Button>
-          <Button onClick={onDismiss} $variant="secondary" type="button">
+          <Button
+            onClick={onDismiss}
+            $variant="secondary"
+            type="button"
+          >
             Cancelar
           </Button>
         </S.ButtonsWrapper>
