3a6173d80d3e285e52c3aaa80c28531f6a848efa
Raquel Sato <raquelsato1@gmail.com>
2024-08-23 17:28:38 -0300

    feat: add search field suggestions on dashboard screen

--- src/pages/Dashboard/components/Searchbar/index.tsx
+++ src/pages/Dashboard/components/Searchbar/index.tsx
@@ -1,18 +1,17 @@
 import { HiRefresh } from "react-icons/hi";
 import { useHistory } from "react-router-dom";
 import Button from "~/components/Buttons/Button";
-import MaskedField from "~/components/MaskedField";
 import routes from "~/router/routes";
 import * as S from "./styles";
 import Loading from "~/components/Loading";
-import { removeCPFMask, validateCPF } from "~/utils/cpf";
 import IconButton from "~/components/Buttons/IconButton";
 import { useContext } from "react";
 import { RegistrationContext } from "~/context/useRegistrationContext";
+import SearchFieldWithSuggestions from "~/components/SearchFieldWithSuggestions";
 
 export const SearchBar = () => {
   const {
-    actions: { setCpf, refetch },
+    actions: { refetch },
     values: { isRefetching },
   } = useContext(RegistrationContext);
 
@@ -22,26 +21,11 @@ export const SearchBar = () => {
     history.push(routes.newUser);
   };
 
-  const handleFilter = (value: string) => {
-    const isValid = validateCPF(value);
-    if (isValid) {
-      return setCpf(removeCPFMask(value));
-    }
-
-    if (!value) {
-      setCpf(value);
-      return refetch();
-    }
-  };
-
   return (
     <S.Container>
-      <MaskedField
+      <SearchFieldWithSuggestions
         mask="999.999.999-99"
         placeholder="Digite um CPF válido"
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          handleFilter(e.target.value)
-        }
       />
       <S.Actions>
         {isRefetching ? (
