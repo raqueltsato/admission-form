3684c24c80e1d86193c185eab0d9e303bfc5ab4d
Raquel Sato <raquelsato1@gmail.com>
2024-08-24 16:34:33 -0300

    fix: providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
+import Toast from "./components/Toast";
 import GlobalProvider from "./providers";
+import Router from "./router";
 
 function App() {
   return (
     <GlobalProvider>
       <Header title="Caju Front Teste" />
+      <Router />
+      <Toast />
     </GlobalProvider>
   );
 }
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,19 +1,13 @@
 import StylesProvider from "./StylesProvider";
 import QueryProvider from "./QueryProvider";
 import { Props } from "./types";
-import Toast from "~/components/Toast";
-import Router from "~/router";
 import { RegistrationsProvider } from "~/context/useRegistrationContext";
 
 const GlobalProvider = ({ children }: Props): JSX.Element => {
   return (
     <StylesProvider>
       <QueryProvider>
-        <RegistrationsProvider>
-          {children}
-          <Router />
-          <Toast />
-        </RegistrationsProvider>
+        <RegistrationsProvider>{children}</RegistrationsProvider>
       </QueryProvider>
     </StylesProvider>
   );
